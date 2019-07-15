5d1e0f893e0310760c095a086c834ea507c0ee29
fabiocon <50738243+fabiocon@users.noreply.github.com>
2019-07-14 22:07:18 -0400

    Add files via upload

--- gps_serial/src/setup_receiver.c
+++ gps_serial/src/setup_receiver.c
@@ -10,6 +10,8 @@
 #include <sys/time.h>
 #include <sys/types.h>
 #include <signal.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
 #define WAIT 5
 void usage();
@@ -23,15 +25,12 @@ int main(int argc, char *argv[])
 // SETTING
 	int gpsuart;
     struct timeval  timeout;
-	//  signal(SIGALRM, usage);
+	  signal(SIGALRM, usage);
 
 
 
 
 	gpsuart=open("/dev/ttyUL1", O_RDWR | O_NOCTTY);
-
-    FD_ZERO(&rfds);
-    FD_SET(gpsuart, &rfds);
 
 
 	 timeout.tv_sec = WAIT;    // WAIT seconds
@@ -39,34 +38,21 @@ int main(int argc, char *argv[])
 
 // WRITING
 	char buffsend[7]={64,64,67,106,41,13,10};
-	write(gpsuart,buffsend,sizeof(buffsend));
+    write(gpsuart,buffsend,sizeof(buffsend));
 
-//sec= alarm(5);
-//printf("sec=%d\n",sec);
-// READING
+alarm(5);
+
 
 		 char read_buffer[10];   /* Buffer to store the data received              */
 		 int  bytes_read = 0;    /* Number of bytes read by the read() system call */
 		 int i = 0;
-	//	bytes_read = read(gpsuart,&read_buffer,5);
-		 bytes_read = select(1, &rfds, NULL, NULL, &timeout);
-	//	 printf("\nBytes Ricevuti %d", bytes_read);
-	//	 printf("\n\n");
+		 alarm(3);
+
+	   	bytes_read = read(gpsuart,&read_buffer,5);
 
 
-
-	//	 for(i=0;i<bytes_read;i++){   /*printing only the received characters*/
-	//		 printf("%c",read_buffer[i]);
-	//	 }
-
-	//	 printf("%c",read_buffer[0]);
-
-	//	 if (read_buffer[0] = "1")
-		 	 if(bytes_read)
 
 				 printf("{\"message\": \"OK OK\"}");
-		 	 else
-		 	printf("{\"message\": \"failed\"}");
 
 
 
@@ -79,9 +65,8 @@ int main(int argc, char *argv[])
 
 void usage()
 {
-//	printf("{\"message\": \"failed\"}");
-	system("killall gps_serial");
-    return 0;
+	printf("{\"message\": \"failed\"}");
+	//system("killall gps_serial");
+    exit(0);
 }
-
 
