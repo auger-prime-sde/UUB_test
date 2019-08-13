6e2dbc96180ba2941d6c55f414423ef217c480e6
fabiocon <50738243+fabiocon@users.noreply.github.com>
2019-08-14 01:19:25 +0200

    Add files via upload

---
+++ record3/Debug/src/record3.d
@@ -0,0 +1,22 @@
+src/record3.o src/record3.d: ../src/record3.c ../src/shwr_evt_defs.h \
+ ../src/xparameters.h ../src/xparameters_ps.h ../src/sde_trigger_defs.h \
+ ../src/sde_trigger.h ../src/xil_types.h ../src/xstatus.h \
+ ../src/sde_trigger_options.h ../src/time_tagging.h
+
+../src/shwr_evt_defs.h:
+
+../src/xparameters.h:
+
+../src/xparameters_ps.h:
+
+../src/sde_trigger_defs.h:
+
+../src/sde_trigger.h:
+
+../src/xil_types.h:
+
+../src/xstatus.h:
+
+../src/sde_trigger_options.h:
+
+../src/time_tagging.h:
