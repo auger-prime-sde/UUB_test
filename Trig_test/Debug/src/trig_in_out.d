56382e724bb91b2829fc6e259e14a7023974a1ad
fabiocon <50738243+fabiocon@users.noreply.github.com>
2021-02-14 20:14:33 +0100

    Add files via upload

---
+++ Trig_test/Debug/src/trig_in_out.d
@@ -0,0 +1,22 @@
+src/trig_in_out.o src/trig_in_out.d: ../src/trig_in_out.c \
+ ../src/shwr_evt_defs.h ../src/xparameters.h ../src/xparameters_ps.h \
+ ../src/sde_trigger_defs.h ../src/sde_trigger.h ../src/xil_types.h \
+ ../src/xstatus.h ../src/sde_trigger_options.h ../src/time_tagging.h
+
+../src/shwr_evt_defs.h:
+
+../src/xparameters.h:
+
+../src/xparameters_ps.h:
+
+../src/sde_trigger_defs.h:
+
+../src/sde_trigger.h:
+
+../src/xil_types.h:
+
+../src/xstatus.h:
+
+../src/sde_trigger_options.h:
+
+../src/time_tagging.h:
